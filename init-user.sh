e6fc3d243e2ef72eeb6699d2b5dfe349016e621c
ak1ra <git@ak1ra.xyz>
2023-08-29 21:12:52 +0800

    update init-user.sh, add unattended-upgrades

--- init-user.sh
+++ init-user.sh
@@ -69,27 +69,8 @@ function init_user() {
 
 function add_ssh_key() {
     local username="$1"
-    local ssh_public_key_content="$2"
 
-    local user_perm=$(awk -F: '$1 ~ /^'$username'/ {print $3":"$4}' /etc/passwd)
-    local user_home_dir=$(awk -F: '$1 ~ /^'$username'/ {print $(NF-1)}' /etc/passwd)
-    local user_ssh_dir="$user_home_dir/.ssh"
-
-    test -d $user_ssh_dir || mkdir -p $user_ssh_dir
-    echo "$ssh_public_key_content" | tee -a $user_ssh_dir/authorized_keys
-
-    chown -R $user_perm $user_ssh_dir
-    find $user_ssh_dir -type d -exec chmod 700 "{}" \;
-    find $user_ssh_dir -type f -exec chmod 600 "{}" \;
-}
-
-function main() {
-    # read required arguments
-    while read -p "Please ENTER the username of the user you want to create: " username; do
-        test -n "$username" && break
-        echo "username can not be empty!"
-    done
-
+    local ssh_public_key=""
     local ssh_public_key_content=""
     local openssh_format_regex='^(ssh-(rsa|ed25519)|ecdsa-sha2-nistp(256|384|521)|(sk-ecdsa-sha2-nistp256|sk-ssh-ed25519)@openssh\.com)'
     while read -p "Please ENTER your ssh_public_key, Can be a GitHub username, OpenSSH format public key url or public key content: " ssh_public_key; do
@@ -99,7 +80,7 @@ function main() {
             break
         fi
 
-        # GitHub username or public key url
+        # Public key url or GitHub username
         local ssh_public_key_url=""
         if echo "$ssh_public_key" | grep -qE '^https?://'; then
             ssh_public_key_url="$ssh_public_key"
@@ -117,11 +98,30 @@ function main() {
         fi
     done
 
+    local user_perm=$(awk -F: '$1 ~ /^'$username'/ {print $3":"$4}' /etc/passwd)
+    local user_home_dir=$(awk -F: '$1 ~ /^'$username'/ {print $(NF-1)}' /etc/passwd)
+    local user_ssh_dir="${user_home_dir}/.ssh"
+
+    test -d $user_ssh_dir || mkdir -p $user_ssh_dir
+    echo "$ssh_public_key_content" | tee -a ${user_ssh_dir}/authorized_keys
+
+    chown -R $user_perm $user_ssh_dir
+    find $user_ssh_dir -type d -exec chmod 700 "{}" \;
+    find $user_ssh_dir -type f -exec chmod 600 "{}" \;
+}
+
+function main() {
+    # read required arguments
+    while read -p "Please ENTER the username of the user you want to create: " username; do
+        test -n "$username" && break
+        echo "username can not be empty!"
+    done
+
     apt_install
     apt_install_extra
 
     init_user "$username"
-    add_ssh_key "$username" "$ssh_public_key_content"
+    add_ssh_key "$username"
 }
 
 main $@
