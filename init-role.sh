cd55fc3789f1f36194fb305cd3b4cf8f87db2785
ak1ra <git@ak1ra.xyz>
2024-05-16 17:42:36 +0800

    make shell script pass shellcheck

--- init-role.sh
+++ init-role.sh
@@ -1,18 +1,23 @@
-#! /bin/bash
+#!/bin/sh
+# shellcheck shell=dash
 
-NAME=$1
-BASE_DIR=$(readlink -f $(dirname "$0"))/roles
+main() {
+    NAME=$1
+    BASE_DIR="$(readlink -f "$(dirname "$0")")/roles"
 
-if [[ $# -ne 1 ]]; then
-    echo "Usage: $0 <role_name>"
-    exit 1
-fi
+    if [ "$#" -ne 1 ]; then
+        echo "Usage: $0 <role_name>"
+        exit 1
+    fi
 
-if [[ -d $BASE_DIR/$NAME ]]; then
-    code $BASE_DIR/$NAME/tasks/main.yml
-else
-    mkdir -p $BASE_DIR/$NAME/{defaults,files,handlers,templates,tasks,vars}
-    tree $BASE_DIR/$NAME
-fi
+    if [ -d "${BASE_DIR}/${NAME}" ]; then
+        code "${BASE_DIR}/${NAME}/tasks/main.yml"
+    else
+        for DIR in defaults files handlers templates tasks vars; do
+            mkdir -p "${BASE_DIR}/${NAME}/${DIR}"
+        done
+        tree "${BASE_DIR}/${NAME}"
+    fi
+}
 
-exit 0
+main "$@"
--- init-user.sh
+++ init-user.sh
@@ -1,16 +1,7 @@
-#! /bin/bash
+#!/bin/bash
+# shellcheck shell=bash
 
-if ! awk -F= '/^ID=/ {print $2}' /etc/os-release | grep -qE '(debian|ubuntu)'; then
-    echo "This script only support Debian/Ubuntu distro."
-    exit 1
-fi
-
-if [ $(id -u) -ne 0 ]; then
-    echo "Please run this script as root or prefix with sudo"
-    exit 2
-fi
-
-function apt_install() {
+apt_install_packages() {
     apt-get update
     apt-get upgrade -y
     apt-get install -y \
@@ -23,20 +14,22 @@ function apt_install() {
         sudo
 }
 
-function apt_install_extra() {
-    local packages="https://raw.githubusercontent.com/ak1ra-lab/selfhosted-server/master/packages.txt"
+apt_install_extra_packages() {
+    mapfile -t extra_packages < <(
+        if [ -f packages.txt ]; then
+            cat packages.txt
+        else
+            curl -s https://raw.githubusercontent.com/ak1ra-lab/selfhosted-server/master/packages.txt
+        fi
+    )
 
     apt-get update
     apt-get upgrade -y
-    if [ -f packages.txt ]; then
-        apt-get install -y $(cat packages.txt)
-    else
-        apt-get install -y $(curl -s $packages)
-    fi
+    apt-get install -y "${extra_packages[@]}"
 }
 
-function init_user() {
-    local username="$1"
+init_user() {
+    username="$1"
     # create sudo group
     if ! grep -q sudo /etc/group; then
         groupadd --system sudo
@@ -48,83 +41,92 @@ function init_user() {
     sed -i 's%^#? *@includedir +/etc/sudoers\.d%@includedir /etc/sudoers.d%' /etc/sudoers
 
     # create user
-    if ! grep -qE '^'$username':' /etc/passwd; then
-        useradd -m -s /bin/bash -G sudo $username
+    if ! grep -qE '^'"${username}"':' /etc/passwd; then
+        useradd -m -s /bin/bash -G sudo "${username}"
     else
-        if ! id $username | grep -q sudo; then
-            usermod -aG sudo $username
+        if ! id "${username}" | grep -q sudo; then
+            usermod -aG sudo "${username}"
         fi
     fi
 
-    local user_home_dir=$(awk -F: '$1 ~ /^'$username'/ {print $(NF-1)}' /etc/passwd)
-    local user_password_file=$user_home_dir/password.txt
+    user_home_dir="$(awk -F: '$1 ~ /^'"${username}"'/ {print $(NF-1)}' /etc/passwd)"
+    user_password_file="${user_home_dir}/password.txt"
 
     # set user password
     # password=$(openssl rand -base64 15)
-    local password=$(dd if=/dev/urandom bs=3 count=5 2>/dev/null | base64)
-    echo "$username:$password" | tee $user_password_file |
-        chpasswd --crypt-method SHA512
-    echo "user_password_file saved at $user_password_file"
+    password="$(dd if=/dev/urandom bs=3 count=5 2>/dev/null | base64)"
+    echo "${username}:${password}" | tee "${user_password_file}" | chpasswd --crypt-method SHA512
+    echo "user_password_file saved at ${user_password_file}"
 }
 
-function add_ssh_key() {
-    local username="$1"
-    local ssh_public_key_content="$2"
+add_ssh_key() {
+    username="$1"
+    ssh_public_key_content="$2"
 
-    local user_perm=$(awk -F: '$1 ~ /^'$username'/ {print $3":"$4}' /etc/passwd)
-    local user_home_dir=$(awk -F: '$1 ~ /^'$username'/ {print $(NF-1)}' /etc/passwd)
-    local user_ssh_dir="$user_home_dir/.ssh"
+    user_perm="$(awk -F: '$1 ~ /^'"${username}"'/ {print $3":"$4}' /etc/passwd)"
+    user_home_dir="$(awk -F: '$1 ~ /^'"${username}"'/ {print $(NF-1)}' /etc/passwd)"
+    user_ssh_dir="${user_home_dir}/.ssh"
 
-    test -d $user_ssh_dir || mkdir -p $user_ssh_dir
-    if ! grep -q "$ssh_public_key_content" ${user_ssh_dir}/authorized_keys; then
-        echo "$ssh_public_key_content" | tee -a ${user_ssh_dir}/authorized_keys
+    test -d "${user_ssh_dir}" || mkdir -p "${user_ssh_dir}"
+    if ! grep -q "${ssh_public_key_content}" "${user_ssh_dir}/authorized_keys"; then
+        echo "${ssh_public_key_content}" | tee -a "${user_ssh_dir}/authorized_keys"
     fi
 
-    chown -R $user_perm $user_ssh_dir
-    find $user_ssh_dir -type d -exec chmod 700 "{}" \;
-    find $user_ssh_dir -type f -exec chmod 600 "{}" \;
+    chown -R "${user_perm}" "${user_ssh_dir}"
+    find "${user_ssh_dir}" -type d -exec chmod 700 "{}" \;
+    find "${user_ssh_dir}" -type f -exec chmod 600 "{}" \;
 }
 
-function main() {
+main() {
+    if ! awk -F= '/^ID=/ {print $2}' /etc/os-release | grep -qE '(debian|ubuntu)'; then
+        echo "This script only support Debian/Ubuntu distro."
+        exit 1
+    fi
+
+    if [ "$(id -u)" -ne 0 ]; then
+        echo "Please run this script as root or prefix with sudo"
+        exit 2
+    fi
+
     # read required arguments
-    while read -p "Please ENTER the username of the user you want to create: " username; do
-        test -n "$username" && break
+    while read -r -p "Please ENTER the username of the user you want to create: " username; do
+        test -n "${username}" && break
         echo "username can not be empty!"
     done
 
-    local ssh_public_key=""
-    local ssh_public_key_content=""
-    local openssh_format_regex='^(ssh-(rsa|ed25519)|ecdsa-sha2-nistp(256|384|521)|(sk-ecdsa-sha2-nistp256|sk-ssh-ed25519)@openssh\.com)'
-    while read -p "Please ENTER your ssh_public_key, Can be a GitHub username, OpenSSH format public key url or public key content: " ssh_public_key; do
+    ssh_public_key=""
+    ssh_public_key_content=""
+    openssh_format_regex='^(ssh-(rsa|ed25519)|ecdsa-sha2-nistp(256|384|521)|(sk-ecdsa-sha2-nistp256|sk-ssh-ed25519)@openssh\.com)'
+    while read -r -p "Please ENTER your ssh_public_key, Can be a GitHub username, OpenSSH format public key url or public key content: " ssh_public_key; do
         # OpenSSH format public key content
-        if echo "$ssh_public_key" | grep -qE "$openssh_format_regex"; then
-            ssh_public_key_content="$ssh_public_key"
+        if echo "${ssh_public_key}" | grep -qE "${openssh_format_regex}"; then
+            ssh_public_key_content="${ssh_public_key}"
             break
         fi
 
         # Public key url or GitHub username
-        local ssh_public_key_url=""
-        if echo "$ssh_public_key" | grep -qE '^https?://'; then
-            ssh_public_key_url="$ssh_public_key"
+        ssh_public_key_url=""
+        if echo "${ssh_public_key}" | grep -qE '^https?://'; then
+            ssh_public_key_url="${ssh_public_key}"
         else
             # arbitrary input, must be a valid GitHub username
             ssh_public_key_url="https://github.com/${ssh_public_key}.keys"
         fi
 
-        ssh_public_key_content="$(curl -sL $ssh_public_key_url)"
-        if echo "$ssh_public_key_content" | grep -qE "$openssh_format_regex"; then
+        ssh_public_key_content="$(curl -sL "${ssh_public_key_url}")"
+        if echo "${ssh_public_key_content}" | grep -qE "${openssh_format_regex}"; then
             break
         else
             echo "ssh_public_key_content format mismatch, please re-ENTER"
-            echo "ssh_public_key_content = $ssh_public_key_content"
+            echo "ssh_public_key_content = ${ssh_public_key_content}"
         fi
     done
 
-    apt_install
-    apt_install_extra
+    apt_install_packages
+    apt_install_extra_packages
 
-    init_user "$username"
-    add_ssh_key "$username" "$ssh_public_key_content"
+    init_user "${username}"
+    add_ssh_key "${username}" "${ssh_public_key_content}"
 }
 
-main $@
+main "$@"
